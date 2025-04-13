fb1536e7076b941bfeb206c43ff709d4cba4bee2
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2025-04-13 23:50:15 +0200

    Refactor import order in product service for improved readability

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './product.entity';
 import { CreateProductDto } from './dto/create-product-dto';
 import { UpdateProductDto } from './dto/update-product-dto';
