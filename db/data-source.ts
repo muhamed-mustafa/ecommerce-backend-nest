f1e1d15b179a2573fe92c14f38784ad587d30c31
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2025-05-01 17:55:35 +0300

    fix: correct import paths in data source configuration

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,18 +1,17 @@
-import { Product } from 'src/product/product.entity';
-import { Review } from 'src/review/review.entity';
-import { User } from 'src/user/user.entity';
+import { Product } from '../src/product/product.entity';
+import { Review } from '../src/review/review.entity';
+import { User } from '../src/user/user.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
 
 config({ path: '.env' });
 
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  entities: [Product, User, Review],
-  migrations: ['dist/db/migrations/*.js'],
-};
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: [User, Product, Review],
+    migrations: ["dist/db/migrations/*.js"]
+}
 
 const dataSource = new DataSource(dataSourceOptions);
-
 export default dataSource;
